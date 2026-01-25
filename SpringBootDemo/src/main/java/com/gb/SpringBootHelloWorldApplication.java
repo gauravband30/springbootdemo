1f2f25343048c92c28e36a145df3b4a1dbdce51d
gauravband30 <gauravband30@gmail.com>
2026-01-25 19:36:37 +0530

    Indentation

--- SpringBootDemo/src/main/java/com/gb/SpringBootHelloWorldApplication.java
+++ SpringBootDemo/src/main/java/com/gb/SpringBootHelloWorldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloWorldApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootHelloWorldApplication.class, args);
     }
 
