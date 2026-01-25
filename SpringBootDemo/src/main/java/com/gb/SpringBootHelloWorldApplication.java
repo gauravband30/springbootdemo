38ece955af6076c1281f7a129cf2c6a42ce000e1
gauravband30 <gauravband30@gmail.com>
2026-01-25 19:24:51 +0530

    InitialCommit

---
+++ SpringBootDemo/src/main/java/com/gb/SpringBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+    }
+
+}
