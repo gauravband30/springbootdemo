63c448b9812f3c236f366c81b89cca85530f69fe
gauravband30 <gauravband30@gmail.com>
2026-03-05 00:24:06 +0530

    enable to external tomcat

--- SpringBootDemo/src/main/java/com/gb/SpringBootHelloWorldApplication.java
+++ SpringBootDemo/src/main/java/com/gb/SpringBootHelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class SpringBootHelloWorldApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
